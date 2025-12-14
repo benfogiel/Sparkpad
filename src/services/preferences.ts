176ff4baec0aae6d06eaff15763a6ebb9f584941
benfogiel <benjaminwfogiel@gmail.com>
2025-12-14 14:20:13 -0800

    use firebase messaging for scheduled notifications + update recentReminders DB
    firebase messaging replaces local notifications for scheduled reminder notifications. We also want to update the recentReminders database when scheduling the first reminder.

--- src/services/preferences.ts
+++ src/services/preferences.ts
@@ -1,29 +1,4 @@
 import { Preferences } from '@capacitor/preferences';
-import { Reminder } from '../data/reminders';
-import { ScheduledReminder } from './notifications';
-
-export const getRecentReminders = async (): Promise<Reminder[]> => {
-  const { value } = await Preferences.get({ key: 'recentReminders' });
-  return JSON.parse(value || '[]');
-};
-
-export const setRecentReminders = async (reminders: Reminder[]) => {
-  await Preferences.set({
-    key: 'recentReminders',
-    value: JSON.stringify(reminders),
-  });
-};
-
-export const addRecentReminder = async (reminder: Reminder) => {
-  const recentReminders = await getRecentReminders();
-  recentReminders.push(reminder);
-  if (
-    recentReminders.length > parseInt(import.meta.env.VITE_MAX_RECENT_REMINDERS || '10')
-  ) {
-    recentReminders.shift();
-  }
-  await setRecentReminders(recentReminders);
-};
 
 export const firstReminderSent = async (): Promise<boolean> => {
   const { value } = await Preferences.get({ key: 'firstReminderSent' });
@@ -32,21 +7,4 @@ export const firstReminderSent = async (): Promise<boolean> => {
 
 export const setFirstReminderSent = async (sent: boolean) => {
   await Preferences.set({ key: 'firstReminderSent', value: sent.toString() });
-};
-
-export const setScheduledReminders = async (reminders: ScheduledReminder[]) => {
-  await Preferences.set({
-    key: 'scheduledReminders',
-    value: JSON.stringify(reminders),
-  });
-};
-
-export const getScheduledReminders = async (): Promise<ScheduledReminder[]> => {
-  const { value } = await Preferences.get({ key: 'scheduledReminders' });
-  const scheduledRemindersJson = JSON.parse(value || '[]');
-  const scheduledReminders = scheduledRemindersJson.map((r: ScheduledReminder) => ({
-    ...r,
-    date: new Date(r.date),
-  }));
-  return scheduledReminders;
 };
